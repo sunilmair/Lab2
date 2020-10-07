829ee8f56b56fed263a6c9f971f53e3dbfe6b5b3
Sunil Mair <sunil.r.mair@gmail.com>
2020-10-06 20:23:12 -0400

    test

--- Ge_script.py
+++ Ge_script.py
@@ -19,6 +19,22 @@ def make_struc(alat):
     structure = Struc(ase2struc(gecell))
     return structure
 
+def make_struc_problem3(alat, z_displacement):
+    """
+    Creates the crystal structure using ASE.
+    :param alat: Lattice parameter in angstrom
+    :return: structure object converted from ase
+    """
+    # set primitive_cell=False if you want to create a simple cubic unit cell with 8 atoms
+    gecell = crystal('Ge', [(0, 0, 0)], spacegroup=227, cellpar=[alat, alat, alat, 90, 90, 90], primitive_cell=True)
+    print(gecell.positions)
+    gecell.positions[0] = (gecell.positions[0][0], gecell.positions[0][1], gecell.position[0][2] + z_displacement*alat)
+    print(gecell.positions)
+    # check how your cell looks like
+    # write('s.cif', gecell)
+    structure = Struc(ase2struc(gecell))
+    return structure
+
 
 def compute_energy(alat, nk, ecut):
     """
@@ -30,7 +46,7 @@ def compute_energy(alat, nk, ecut):
     pseudopots = {'Ge': PseudoPotential(name=potname, path=potpath, ptype='uspp', element='Ge', functional='LDA')}
     struc = make_struc(alat=alat)
     kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
-    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab2/Problem2/t2", str(nk)))
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab2/Problem3/t1", str(nk)))
     input_params = PWscf_inparam({
         'CONTROL': {
             'calculation': 'scf',
@@ -58,13 +74,51 @@ def compute_energy(alat, nk, ecut):
     return output
 
 
+def compute_energy_problem3(alat, z_displacement, nk, ecut):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    potname = 'Ge.pz-bhs.UPF'
+    pseudopath = os.environ['ESPRESSO_PSEUDO']
+    potpath = os.path.join(pseudopath, potname)
+    pseudopots = {'Ge': PseudoPotential(name=potname, path=potpath, ptype='uspp', element='Ge', functional='LDA')}
+    struc = make_struc_problem3(alat=alat, z_displacement=z_displacement)
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab2/Problem3/t1", str(nk)))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'scf',
+            'pseudo_dir': pseudopath,
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none',
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.5,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {},
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
 def lattice_scan():
     nk = 9
     ecut = 30.0
     alat = 5.0
     output = compute_energy(alat=alat, ecut=ecut, nk=nk)
+    print(output)
     energy = output['energy']
-    print(energy)
+    #print(energy)
 
 def problem1():
     #change runpathline in compute_energy to:
@@ -84,6 +138,17 @@ def problem2():
     alat = 5.0
     output = [compute_energy(alat=alat, ecut=ecut, nk=nk) for nk in nk_list]
 
+def problem3():
+
+    nk = 4
+    ecut_list = np.arange(5.0, 85.0, 5.0)
+    alat = 5.0
+    z_displacement = 0.05
+    output = [compute_energy(alat=alat, z_displacement=z_displacement, ecut=ecut, nk=nk) for ecut in ecut_list]
+
+
 if __name__ == '__main__':
     # put here the function that you actually want to run
-    problem2()
+    make_struc_problem3(5.0, 0.05) #check if struc is good
+    #lattice_scan() #check if 'force' is in 'output'
+    #problem3()
