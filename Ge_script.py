37aa46cb3ffb91aedefbe5763567b950ca3b5cc7
Sunil Mair <sunil.r.mair@gmail.com>
2020-10-07 02:50:22 -0400

    test

--- Ge_script.py
+++ Ge_script.py
@@ -19,6 +19,7 @@ def make_struc(alat):
     structure = Struc(ase2struc(gecell))
     return structure
 
+
 def make_struc_problem3(alat, z_displacement):
     """
     Creates the crystal structure using ASE.
@@ -72,6 +73,59 @@ def compute_energy(alat, nk, ecut):
     return output
 
 
+def relax_struc_problem7c(alat, nk, ecut, forc_conv_thr, press_conv_thr):
+    #in CONTROL
+    #'calculation': 'vc-relax'
+    #'forc_conv_thr'
+    #in IONS
+    #'ion_dynamics': 'bfgs'
+    #in CELL
+    #all of it
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    potname = 'Ge.pz-bhs.UPF'
+    pseudopath = os.environ['ESPRESSO_PSEUDO']
+    potpath = os.path.join(pseudopath, potname)
+    pseudopots = {'Ge': PseudoPotential(name=potname, path=potpath, ptype='uspp', element='Ge', functional='LDA')}
+    struc = make_struc(alat=alat)
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab2/Problem7c/t1", str(alat)))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'vc-relax',
+            'forc_conv_thr': forc_conv_thr
+            'pseudo_dir': pseudopath,
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none',
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.5,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {
+            'ion_dynamics': 'bfgs',
+        },
+        'CELL': {
+            'cell_dofree': 'all',
+            'cell_dynamics': 'bfgs',
+            'press': 0.0,
+            'press_conv_thr': press_conv_thr,
+        },
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
+
 def compute_energy_problem3(alat, z_displacement, nk, ecut):
     """
     Make an input template and select potential and structure, and the path where to run
@@ -109,6 +163,7 @@ def compute_energy_problem3(alat, z_displacement, nk, ecut):
     output = parse_qe_pwscf_output(outfile=output_file)
     return output
 
+
 def lattice_scan():
     nk = 9
     ecut = 30.0
@@ -118,6 +173,7 @@ def lattice_scan():
     energy = output['energy']
     #print(energy)
 
+
 def problem1():
     #change runpathline in compute_energy to:
     #runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab2/Problem1/t1", str(ecut)))
@@ -126,6 +182,7 @@ def problem1():
     ecut_list = np.arange(5.0, 85.0, 5.0)
     alat = 5.0
     output = [compute_energy(alat=alat, ecut=ecut, nk=nk) for ecut in ecut_list]
+
 
 def problem2():
     # change runpathline in compute_energy to:
@@ -174,6 +231,18 @@ def problem7a():
     alat_list = np.arange(5.4, 5.825, 0.025)
     output = [compute_energy(alat=alat, ecut=ecut, nk=nk) for alat in alat_list]
 
+
+def problem7c():
+
+    nk = 4
+    ecut = 30
+    alat = 5.0
+
+    forc_conv_thr = 0.001
+    press_conv_thr = 0.5
+
+    output = relax_struc_problem7c(alat, nk, ecut, forc_conv_thr, press_conv_thr)
+
 if __name__ == '__main__':
     # put here the function that you actually want to run
-    problem7a()
+    problem7c()
